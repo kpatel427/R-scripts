3da3df2ad4baa98a858563014830d9d2fb43da86
Khushbu Patel <kpatel427@gatech.edu>
2019-03-27 14:51:15 -0400

    update

--- data-wrangling.r
+++ data-wrangling.r
@@ -1,19 +1,19 @@
 library(ggplot2)
 library(ggforce)
-data <- read.delim("2018-11-13_FPKM_3_transcripts_per_exon.txt")
+data <- read.delim("FPKM_3_transcripts_per_exon.txt")
 
-sub_dat <- subset(data, data$Sample == "COG-N-625x-D", c(colnames(data)))
-tx_dat <- subset(sub_dat, sub_dat$transcript == "ENST00000480283.1", c(colnames(sub_dat)))
+sub_dat <- subset(data, data$Sample == "COG-N-100x-D", c(colnames(data)))
+tx_dat <- subset(sub_dat, sub_dat$transcript == "ENST000000000.1", c(colnames(sub_dat)))
 
 
 a <- ggplot(sub_dat, aes(exon_n, FPKM)) +
   geom_bar(aes(fill = transcript),stat = "identity") +
   scale_x_continuous(breaks = pretty(sub_dat$exon_n, n = 35)) +
-  labs(title = "FPKM for exons in model COG-N-625x") +
+  labs(title = "FPKM for exons in model COG-N-100x") +
   facet_wrap(. ~ sub_dat$transcript, nrow = 3) +
   theme_bw()
 
-ggsave(paste0(Sys.Date(),"-exon-number-all-TXs-COG-N-625x-.pdf"), a, width=45, height=20, units = "cm")
+ggsave(paste0(Sys.Date(),"-exon-number-all-TXs-COG-N-100x-.pdf"), a, width=45, height=20, units = "cm")
 
 
 # Grepping pattern to get all rows matching the string
@@ -32,7 +32,7 @@ ggplot(nbl, aes(exon_n, FPKM)) +
 
 # total number of Models = 26
 n_pages_needed = 13
-pdf("2019-01-03-NBL-Tx-ATRX.pdf", width=15, height=9)
+pdf("NBL-Tx-ATRX.pdf", width=15, height=9)
 
 for (i in seq_len(n_pages_needed)) {
   print(ggplot(nbl,aes(nbl$exon_n, nbl$FPKM, group = transcript, color = transcript)) +
