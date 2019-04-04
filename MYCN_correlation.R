70691797963944857f178f127ba16822cf847f65
Khushbu Patel <kpatel427@gatech.edu>
2019-04-04 11:23:19 -0400

    upload

--- MYCN_correlation.R
+++ MYCN_correlation.R
@@ -45,7 +45,7 @@ sample.mycn.risk$Sample_title <- gsub(" ","",sample.mycn.risk$Sample_title, fixe
 
 
 
-# GOI gene list
+# Kristen's gene list
 gene <- scan("identified_genelist.txt", what = "", sep = ",")
 df.gene <- as.data.frame(gene)
 
@@ -71,6 +71,8 @@ ggplot(matrix.genelist, aes(x = X00gene_id, y = value, fill = high_risk)) +
   labs(title = "FPKM expression at different risk levels", x = "Genes", y = "FPKM") +
   guides(fill=guide_legend(title= "Risk")) +
   theme_bw()
+
+ggsave("boxplot-FPKM-risk-levels.pdf", width = 15, height = 9)
   
 # boxplots showing FPKM expression for mycn_status for all genes in gene list
 ggplot(matrix.genelist, aes(x = X00gene_id, y = value, fill = mycn_status)) +
@@ -78,6 +80,7 @@ ggplot(matrix.genelist, aes(x = X00gene_id, y = value, fill = mycn_status)) +
   labs(title = "FPKM expression for mycn_status", x = "Genes", y = "FPKM") +
   guides(fill=guide_legend(title= "MYCN status")) +
   theme_bw()
+ggsave("boxplot-FPKM-mycn-status.pdf", width = 15, height = 9)
 
 
 # correlation of MYCN expression with the rest of the genes
@@ -92,7 +95,7 @@ levels(matrix.genelist.subset$high_risk) <- c("Low Risk","High Risk")
 levels(matrix.genelist.subset$mycn_status) <-  c("Non Amplified", "Amplified", "N/A")
 
 
-# plotting correlation ~ mycn_statis
+# plotting correlation ~ mycn_status
 ggscatter(matrix.genelist.subset, x = "value", y = "mycn_expression_value",
           shape = 1, alpha = 0.6,
           add = "reg.line", conf.int = TRUE,
@@ -104,6 +107,7 @@ ggscatter(matrix.genelist.subset, x = "value", y = "mycn_expression_value",
   facet_wrap(~mycn_status, scales="free_x") +
   labs(x = "Genes of interest", y = "MYCN expression") + 
   theme_bw()
+ggsave("correlation-mycn-status.pdf", width = 10, height = 6)
 
 
 # plotting correlation ~ Risk
@@ -118,11 +122,77 @@ ggscatter(matrix.genelist.subset, x = "value", y = "mycn_expression_value",
   facet_wrap(~high_risk, scales="free_x") +
   labs(x = "Genes of interest", y = "MYCN expression") + 
   theme_bw()
+ggsave("correlation-risk-levels.pdf", width = 10, height = 6)
 
 
-ggplot(matrix.genelist.subset) +
-  geom_jitter(aes(value, mycn_expression_value, colour=mycn_status),) + 
-  geom_smooth(aes(value, mycn_expression_value, colour=mycn_status), method=lm, se=FALSE) +
-  facet_wrap(~mycn_status, scales="free_x") +
-  labs(x = "Genes of interest", y = "MYCN expression")
+# ggplot(matrix.genelist.subset) +
+#   geom_jitter(aes(value, mycn_expression_value, colour=mycn_status),) + 
+#   geom_smooth(aes(value, mycn_expression_value, colour=mycn_status), method=lm, se=FALSE) +
+#   facet_wrap(~mycn_status, scales="free_x") +
+#   labs(x = "Genes of interest", y = "MYCN expression")
+
+
+#------------------------------------------ Wilcox's test -----------------------------------------------------------#
+# Comparing the means of two independent groups (high risk and low risk)
+# by Risk := comparing high risk (amp/non-amp) vs low risk (amp/non-amp)
+
+#----------------------- Risk ------------------------------#
+# 1. High risk amplified : Low risk amplified
+# there is no low risk amplified expression for the set of genes provided
+
+# 2. High risk non-amplified : Low risk non-amplified
+
+risk.nonamp <- subset(matrix.genelist, matrix.genelist$mycn_status == " 0", select = c(colnames(matrix.genelist)) )
+
+res1 <-  wilcox.test(value ~ high_risk, data = risk.nonamp,
+            exact = FALSE)
+res1$p.value
+
+# p-value = 0.4607 which is not less than 0.05, 
+# so there is no significant difference in median expression levels of genes between High risk non-amplified & Low risk non-amplified
+
+# Renaming factor levels
+levels(risk.nonamp$high_risk) <- c("Low Risk","High Risk")
+
+# plot
+ggboxplot(risk.nonamp, x = "high_risk", y = "value",
+          color = "high_risk", palette = "jco") + 
+          stat_compare_means()+ 
+          stat_compare_means(method = "wilcox.test") +
+          labs(title = "For non-amplified samples", x = "Risk", y = "FPKM") +
+          theme(legend.title = element_blank(), legend.position = "none")
+
+ggsave("wilcox-highrisk.pdf", width = 7, height = 5)
+
+
+#----------------------- Amplification status ------------------------------#
+# Comparing the means of two independent groups (Amp and non-amp)
+# by Amplification status := comparing Amp (high risk/low risk) vs non-amp (high risk/low risk)
+
+# 3. High risk amplified : High risk non-amplified
+amp.highrisk <- subset(matrix.genelist, matrix.genelist$high_risk == " 1" & matrix.genelist$mycn_status != " N/A", select = c(colnames(matrix.genelist)) )
+
+res2 <-  wilcox.test(value ~ mycn_status, data = amp.highrisk,
+                     exact = FALSE)
+res2$p.value
+# p-value = 0.000000000004442 which is significantly less than 0.05, 
+# so there is significant difference in median expression levels of genes between high rosk amplified and non-amplified patients
+
+# Renaming factor levels
+levels(amp.highrisk$mycn_status) <- c("Non-amplified","Amplified","N/A")
+
+# plot
+ggboxplot(amp.highrisk, x = "mycn_status", y = "value",
+          color = "mycn_status", palette = "jco") + 
+          stat_compare_means()+ 
+          stat_compare_means(method = "wilcox.test") +
+          labs(title = "For High-risk samples", x = "MYCN status", y = "FPKM")+
+          theme(legend.title = element_blank(), legend.position = "none")
+
+ggsave("wilcox-mycn-status.pdf", width = 7, height = 5)
+
+
+# 4. low risk amplified : low risk non-amplified
+# there is no low risk amplified expression for the set of genes provided
+
 
