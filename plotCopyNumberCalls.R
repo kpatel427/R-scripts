339b1ded8329165cfb2ab95570c6e68d490ffa5e
Khushbu Patel <kpatel427@gatech.edu>
2021-01-26 16:41:51 -0500

    update

--- plotCopyNumberCalls.R
+++ plotCopyNumberCalls.R
@@ -182,4 +182,90 @@ legend("topright", legend=names(cn.cols), fill = cn.cols, ncol=length(cn.cols), 
        horiz = TRUE, col = c(cn.cols), bty = "n", cex = 0.5, x.intersp=0.5, xjust=0, yjust=0)
 dev.off()
 
+# adding gene annotations to plots ------------------
+# 1. MYCN
+gene.symbols <- c(goi$gene)
+
+ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+genes<-getBM(c("ensembl_gene_id", "mgi_symbol", "chromosome_name", 
+               "strand", "start_position", "end_position","gene_biotype"), 
+             mart=ensembl)
+
+# subset
+genes.new <- genes[,c(2:6)]
+# fix gene column name
+genes.new$mgi_symbol <- toupper(genes.new$mgi_symbol)
+
+# merge with genes of interest list
+genes.new <- merge(goi, genes.new, by.x = 'gene', by.y = 'mgi_symbol', all.x = TRUE)
+# fix Chromosome column
+genes.new$chromosome_name <- gsub('(.*)','chr\\1', genes.new$chromosome_name)
+
+# convert to GRanges 
+genes.new <- na.omit(genes.new)
+genes1 <- makeGRangesFromDataFrame(genes.new,
+                                   keep.extra.columns=F,
+                                   ignore.strand=TRUE,
+                                   seqinfo=NULL,
+                                   seqnames.field=c("seqnames", "seqname",
+                                                    "chromosome", "chrom",
+                                                    "chr", "chromosome_name",
+                                                    "seqid"),
+                                   start.field="start_position",
+                                   end.field="end_position",
+                                   strand.field="strand",
+                                   starts.in.df.are.0based=FALSE)
+
+values(genes1) <- data.frame(gene=genes.new$gene)
+seqlevelsStyle(genes1) <- "UCSC"
+
+head(genes)
+
+#kp <- plotKaryotype(genome="hg38")
+#kpPlotMarkers(kp, data=genes, labels=genes$hgnc_symbol)
+
+plot.new()
+kp <- plotKaryotype(genome = 'mm10', plot.type = 1, chromosomes = paste('chr',chrs[1:11], sep = ''))
+kpAddCytobandLabels(kp)
+cn.cols <- getCopyNumberColors(colors = "red_blue")
+plotCopyNumberCalls(kp, cn.calls1, cn.colors = cn.cols, ymin = -1, ymax=10, r0=0, r1=1, loh.height = 10, label.cex = 0.5) 
+kpPlotMarkers(kp, data=genes1, labels=genes1$gene,text.orientation = "horizontal",
+              r1=0.5, cex=0.8, adjust.label.position = FALSE)
+legend("topright", legend=names(cn.cols), fill = cn.cols, ncol=length(cn.cols), inset = c(-0.2,5), xpd = TRUE, 
+       horiz = TRUE, col = c(cn.cols), bty = "n", cex = 0.4, x.intersp=0.5, xjust=0, yjust=0)
+
+
+
+plot.new()
+kp <- plotKaryotype(genome = 'mm10', plot.type = 1, chromosomes = paste('chr',chrs[12:21], sep = ''))
+kpAddCytobandLabels(kp)
+cn.cols <- getCopyNumberColors(colors = "red_blue")
+plotCopyNumberCalls(kp, cn.calls1, cn.colors = cn.cols, ymin = -1, ymax=10, r0=0, r1=1, loh.height = 10, label.cex = 0.5) 
+kpPlotMarkers(kp, data=genes1, labels=genes1$gene,text.orientation = "horizontal",
+              r1=0.5, cex=0.8, adjust.label.position = FALSE)
+legend("topright", legend=names(cn.cols), fill = cn.cols, ncol=length(cn.cols), inset = c(-0.2,5), xpd = TRUE, 
+       horiz = TRUE, col = c(cn.cols), bty = "n", cex = 0.4, x.intersp=0.5, xjust=0, yjust=0)
+
+
+
+# 2. c-MYC
+plot.new()
+kp <- plotKaryotype(genome = 'mm10', plot.type = 1, chromosomes = paste('chr',chrs[1:11], sep = ''))
+kpAddCytobandLabels(kp)
+cn.cols <- getCopyNumberColors(colors = "red_blue")
+plotCopyNumberCalls(kp, cn.calls.cmyc, cn.colors = cn.cols, ymin = -1, ymax=10, r0=0, r1=1, loh.height = 10, label.cex = 0.5)
+kpPlotMarkers(kp, data=genes1, labels=genes1$gene,text.orientation = "horizontal",
+              r1=0.5, cex=0.8, adjust.label.position = FALSE)
+legend("topright", legend=names(cn.cols), fill = cn.cols, ncol=length(cn.cols), inset = c(-0.2,5), xpd = TRUE, 
+       horiz = TRUE, col = c(cn.cols), bty = "n", cex = 0.4, x.intersp=0.5, xjust=0, yjust=0)
+
+plot.new()
+kp <- plotKaryotype(genome = 'mm10', plot.type = 1, chromosomes = paste('chr',chrs[12:21], sep = ''))
+kpAddCytobandLabels(kp)
+cn.cols <- getCopyNumberColors(colors = "red_blue")
+plotCopyNumberCalls(kp, cn.calls.cmyc, cn.colors = cn.cols, ymin = -1, ymax=10, r0=0, r1=1, loh.height = 10, label.cex = 0.5)
+kpPlotMarkers(kp, data=genes1, labels=genes1$gene,text.orientation = "horizontal",
+              r1=0.5, cex=0.8, adjust.label.position = FALSE)
+legend("topright", legend=names(cn.cols), fill = cn.cols, ncol=length(cn.cols), inset = c(-0.2,5), xpd = TRUE, 
+       horiz = TRUE, col = c(cn.cols), bty = "n", cex = 0.4, x.intersp=0.5, xjust=0, yjust=0)
 
